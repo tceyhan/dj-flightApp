85b0407d6004a261be4b032bd1e72906fbbb898b
tceyhan <55tc155@gmail.com>
2022-06-27 01:12:27 +0300

    project started drf template plus uses app and dj-rest-auth 3rd party apps added

---
+++ users/serializers.py
@@ -0,0 +1,44 @@
+from asyncore import write
+from wsgiref.validate import validator
+from rest_framework import serializers, validators
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    #! uniq ve required olsun diye email field custom yapıyoruz.
+    email = serializers.EmailField(
+        required = True,
+        validators = [validators.UniqueValidator(queryset=User.objects.all())] 
+    )
+    password = serializers.CharField(
+        write_only = True,
+        # required = True,        
+        validators = [validate_password],
+        style = {"input-type": "password"}
+    )
+    password2 = serializers.CharField(
+        write_only = True,
+        required = True,        
+        validators = [validate_password],
+        style = {"input-type": "password"}
+    )
+    
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+            "password2",            
+        )
+    
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        validated_data.pop("password2")
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
